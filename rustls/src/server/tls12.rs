jglick/rustls

server: inline trivial wrapper type ClientCertDetails

rustls/src/server/tls12.rs
@@ -23,7 +23,7 @@ use crate::session::{SessionRandoms, SessionSecrets};
 use crate::sign;
 use crate::verify;
 
-use crate::server::common::{ClientCertDetails, HandshakeDetails, ServerKXDetails};
+use crate::server::common::{HandshakeDetails, ServerKXDetails};
 use crate::server::hs;
 
 use ring::constant_time;
@@ -267,7 +267,7 @@ impl hs::State for ExpectCertificate {
                         Err(err)
                     })?;
 
-                Some(ClientCertDetails::new(cert_chain.clone()))
+                Some(cert_chain.clone())
             }
         };
 
@@ -288,7 +288,7 @@ pub struct ExpectClientKX {
     pub randoms: SessionRandoms,
     pub using_ems: bool,
     pub server_kx: ServerKXDetails,
-    pub client_cert: Option<ClientCertDetails>,
+    pub client_cert: Option<Vec<Certificate>>,
     pub send_ticket: bool,
 }
 
@@ -362,7 +362,7 @@ pub struct ExpectCertificateVerify {
     secrets: SessionSecrets,
     handshake: HandshakeDetails,
     using_ems: bool,
-    client_cert: ClientCertDetails,
+    client_cert: Vec<Certificate>,
     send_ticket: bool,
 }
 
@@ -382,7 +382,7 @@ impl hs::State for ExpectCertificateVerify {
                 .handshake
                 .transcript
                 .take_handshake_buf();
-            let certs = &self.client_cert.cert_chain;
+            let certs = &self.client_cert;
 
             sess.config
                 .get_verifier()
@@ -396,7 +396,7 @@ impl hs::State for ExpectCertificateVerify {
         }
 
         trace!("client CertificateVerify OK");
-        sess.client_cert_chain = Some(self.client_cert.cert_chain);
+        sess.client_cert_chain = Some(self.client_cert);
 
         self.handshake
             .transcript

rustls/src/server/tls13.rs
@@ -44,7 +44,7 @@ use crate::{cipher, SupportedCipherSuite};
 #[cfg(feature = "quic")]
 use crate::{msgs::handshake::NewSessionTicketExtension, quic, session::Protocol};
 
-use crate::server::common::{ClientCertDetails, HandshakeDetails};
+use crate::server::common::HandshakeDetails;
 use crate::server::hs;
 
 use ring::constant_time;
@@ -725,7 +725,7 @@ impl hs::State for ExpectCertificate {
             ));
         }
 
-        let cert_chain = certp.convert();
+        let client_cert = certp.convert();
 
         let mandatory = sess
             .config
@@ -738,7 +738,7 @@ impl hs::State for ExpectCertificate {
                 TlsError::General("client rejected by client_auth_mandatory".into())
             })?;
 
-        let (end_entity, intermediates) = match cert_chain.split_first() {
+        let (end_entity, intermediates) = match client_cert.split_first() {
             None => {
                 if !mandatory {
                     debug!("client auth requested but no certificate supplied");
@@ -770,7 +770,6 @@ impl hs::State for ExpectCertificate {
                 Err(err)
             })?;
 
-        let client_cert = ClientCertDetails::new(cert_chain);
         Ok(Box::new(ExpectCertificateVerify {
             handshake: self.handshake,
             randoms: self.randoms,
@@ -786,7 +785,7 @@ pub struct ExpectCertificateVerify {
     handshake: HandshakeDetails,
     randoms: SessionRandoms,
     key_schedule: KeyScheduleTrafficWithClientFinishedPending,
-    client_cert: ClientCertDetails,
+    client_cert: Vec<Certificate>,
     send_ticket: bool,
     hash_at_server_fin: Digest,
 }
@@ -810,7 +809,7 @@ impl hs::State for ExpectCertificateVerify {
             self.handshake
                 .transcript
                 .abandon_client_auth();
-            let certs = &self.client_cert.cert_chain;
+            let certs = &self.client_cert;
             let msg = verify::construct_tls13_client_verify_message(&handshake_hash);
 
             sess.config
@@ -825,7 +824,7 @@ impl hs::State for ExpectCertificateVerify {
         }
 
         trace!("client CertificateVerify OK");
-        sess.client_cert_chain = Some(self.client_cert.cert_chain);
+        sess.client_cert_chain = Some(self.client_cert);
 
         self.handshake
             .transcript

lf-/clipper

rustls/src/server/tls12.rs
@@ -22,7 +22,7 @@ use crate::sign;
 use crate::verify;
 use crate::SupportedCipherSuite;
 
-use crate::server::common::{ClientCertDetails, HandshakeDetails, ServerKxDetails};
+use crate::server::common::{HandshakeDetails, ServerKxDetails};
 use crate::server::hs;
 
 use ring::constant_time;
@@ -277,7 +277,7 @@ impl hs::State for ExpectCertificate {
                         err
                     })?;
 
-                Some(ClientCertDetails::new(cert_chain))
+                Some(cert_chain)
             }
         };
 
@@ -300,7 +300,7 @@ pub struct ExpectClientKx {
     pub suite: &'static SupportedCipherSuite,
     pub using_ems: bool,
     pub server_kx: ServerKxDetails,
-    pub client_cert: Option<ClientCertDetails>,
+    pub client_cert: Option<Vec<Certificate>>,
     pub send_ticket: bool,
 }
 
@@ -378,7 +378,7 @@ pub struct ExpectCertificateVerify {
     secrets: ConnectionSecrets,
     handshake: HandshakeDetails,
     using_ems: bool,
-    client_cert: ClientCertDetails,
+    client_cert: Vec<Certificate>,
     send_ticket: bool,
 }
 
@@ -398,7 +398,7 @@ impl hs::State for ExpectCertificateVerify {
                 .handshake
                 .transcript
                 .take_handshake_buf();
-            let certs = &self.client_cert.cert_chain;
+            let certs = &self.client_cert;
 
             conn.config
                 .get_verifier()
@@ -412,7 +412,7 @@ impl hs::State for ExpectCertificateVerify {
         }
 
         trace!("client CertificateVerify OK");
-        conn.client_cert_chain = Some(self.client_cert.cert_chain);
+        conn.client_cert_chain = Some(self.client_cert);
 
         self.handshake
             .transcript

rustls/src/server/tls13.rs
@@ -44,7 +44,7 @@ use crate::{cipher, SupportedCipherSuite};
 #[cfg(feature = "quic")]
 use crate::{conn::Protocol, msgs::handshake::NewSessionTicketExtension, quic};
 
-use crate::server::common::{ClientCertDetails, HandshakeDetails};
+use crate::server::common::HandshakeDetails;
 use crate::server::hs;
 
 use ring::constant_time;
@@ -742,7 +742,7 @@ impl hs::State for ExpectCertificate {
             ));
         }
 
-        let cert_chain = certp.convert();
+        let client_cert = certp.convert();
 
         let mandatory = conn
             .config
@@ -755,7 +755,7 @@ impl hs::State for ExpectCertificate {
                 Error::General("client rejected by client_auth_mandatory".into())
             })?;
 
-        let (end_entity, intermediates) = match cert_chain.split_first() {
+        let (end_entity, intermediates) = match client_cert.split_first() {
             None => {
                 if !mandatory {
                     debug!("client auth requested but no certificate supplied");
@@ -788,7 +788,6 @@ impl hs::State for ExpectCertificate {
                 err
             })?;
 
-        let client_cert = ClientCertDetails::new(cert_chain);
         Ok(Box::new(ExpectCertificateVerify {
             suite: self.suite,
             handshake: self.handshake,
@@ -806,7 +805,7 @@ pub struct ExpectCertificateVerify {
     suite: &'static SupportedCipherSuite,
     randoms: ConnectionRandoms,
     key_schedule: KeyScheduleTrafficWithClientFinishedPending,
-    client_cert: ClientCertDetails,
+    client_cert: Vec<Certificate>,
     send_ticket: bool,
     hash_at_server_fin: Digest,
 }
@@ -830,7 +829,7 @@ impl hs::State for ExpectCertificateVerify {
             self.handshake
                 .transcript
                 .abandon_client_auth();
-            let certs = &self.client_cert.cert_chain;
+            let certs = &self.client_cert;
             let msg = verify::construct_tls13_client_verify_message(&handshake_hash);
 
             conn.config
@@ -845,7 +844,7 @@ impl hs::State for ExpectCertificateVerify {
         }
 
         trace!("client CertificateVerify OK");
-        conn.client_cert_chain = Some(self.client_cert.cert_chain);
+        conn.client_cert_chain = Some(self.client_cert);
 
         self.handshake
             .transcript