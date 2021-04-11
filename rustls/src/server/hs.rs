jglick/rustls

server: detach emit_certificate() from ExpectClientHello

rustls/src/server/hs.rs
@@ -1,5 +1,4 @@
 use crate::error::TlsError;
-use crate::key::Certificate;
 use crate::kx;
 #[cfg(feature = "logging")]
 use crate::log::{debug, trace};
@@ -336,22 +335,6 @@ impl ExpectClientHello {
         }
 
         ech
-    }
-
-    fn emit_certificate(&mut self, sess: &mut ServerSessionImpl, cert_chain: &[Certificate]) {
-        let c = Message {
-            typ: ContentType::Handshake,
-            version: ProtocolVersion::TLSv1_2,
-            payload: MessagePayload::Handshake(HandshakeMessagePayload {
-                typ: HandshakeType::Certificate,
-                payload: HandshakePayload::Certificate(cert_chain.to_owned()),
-            }),
-        };
-
-        self.handshake
-            .transcript
-            .add_message(&c);
-        sess.common.send_msg(c, false);
     }
 
     fn emit_cert_status(&mut self, sess: &mut ServerSessionImpl, ocsp: &[u8]) {
@@ -866,7 +849,7 @@ impl State for ExpectClientHello {
             None,
             &randoms,
         )?;
-        self.emit_certificate(sess, &certkey.cert);
+        tls12::emit_certificate(&mut self.handshake, sess, &certkey.cert);
         if let Some(ocsp_response) = ocsp_response {
             self.emit_cert_status(sess, ocsp_response);
         }

rustls/src/server/tls12.rs
@@ -1,5 +1,6 @@
 use crate::check::check_message;
 use crate::error::TlsError;
+use crate::key::Certificate;
 #[cfg(feature = "logging")]
 use crate::log::{debug, trace};
 use crate::msgs::base::Payload;
@@ -55,6 +56,24 @@ pub(super) fn emit_server_hello(
     handshake.transcript.add_message(&sh);
     sess.common.send_msg(sh, false);
     Ok(ep.send_ticket)
+}
+
+pub(super) fn emit_certificate(
+    handshake: &mut HandshakeDetails,
+    sess: &mut ServerSessionImpl,
+    cert_chain: &[Certificate],
+) {
+    let c = Message {
+        typ: ContentType::Handshake,
+        version: ProtocolVersion::TLSv1_2,
+        payload: MessagePayload::Handshake(HandshakeMessagePayload {
+            typ: HandshakeType::Certificate,
+            payload: HandshakePayload::Certificate(cert_chain.to_owned()),
+        }),
+    };
+
+    handshake.transcript.add_message(&c);
+    sess.common.send_msg(c, false);
 }
 
 // --- Process client's Certificate for client auth ---

lf-/clipper

rustls/src/server/hs.rs
@@ -2,7 +2,6 @@
 use crate::conn::Protocol;
 use crate::conn::{ConnectionRandoms, ConnectionSecrets};
 use crate::error::Error;
-use crate::key::Certificate;
 use crate::kx;
 #[cfg(feature = "logging")]
 use crate::log::{debug, trace};
@@ -336,22 +335,6 @@ impl ExpectClientHello {
         }
 
         ech
-    }
-
-    fn emit_certificate(&mut self, conn: &mut ServerConnection, cert_chain: &[Certificate]) {
-        let c = Message {
-            typ: ContentType::Handshake,
-            version: ProtocolVersion::TLSv1_2,
-            payload: MessagePayload::Handshake(HandshakeMessagePayload {
-                typ: HandshakeType::Certificate,
-                payload: HandshakePayload::Certificate(cert_chain.to_owned()),
-            }),
-        };
-
-        self.handshake
-            .transcript
-            .add_message(&c);
-        conn.common.send_msg(c, false);
     }
 
     fn emit_cert_status(&mut self, conn: &mut ServerConnection, ocsp: &[u8]) {
@@ -860,7 +843,7 @@ impl State for ExpectClientHello {
             None,
             &randoms,
         )?;
-        self.emit_certificate(conn, &certkey.cert);
+        tls12::emit_certificate(&mut self.handshake, conn, &certkey.cert);
         if let Some(ocsp_response) = ocsp_response {
             self.emit_cert_status(conn, ocsp_response);
         }

rustls/src/server/tls12.rs
@@ -1,6 +1,7 @@
 use crate::check::check_message;
 use crate::conn::{ConnectionRandoms, ConnectionSecrets};
 use crate::error::Error;
+use crate::key::Certificate;
 #[cfg(feature = "logging")]
 use crate::log::{debug, trace};
 use crate::msgs::base::Payload;
@@ -64,6 +65,24 @@ pub(super) fn emit_server_hello(
     handshake.transcript.add_message(&sh);
     conn.common.send_msg(sh, false);
     Ok(ep.send_ticket)
+}
+
+pub(super) fn emit_certificate(
+    handshake: &mut HandshakeDetails,
+    conn: &mut ServerConnection,
+    cert_chain: &[Certificate],
+) {
+    let c = Message {
+        typ: ContentType::Handshake,
+        version: ProtocolVersion::TLSv1_2,
+        payload: MessagePayload::Handshake(HandshakeMessagePayload {
+            typ: HandshakeType::Certificate,
+            payload: HandshakePayload::Certificate(cert_chain.to_owned()),
+        }),
+    };
+
+    handshake.transcript.add_message(&c);
+    conn.common.send_msg(c, false);
 }
 
 // --- Process client's Certificate for client auth ---