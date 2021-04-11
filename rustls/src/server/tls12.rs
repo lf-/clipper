jglick/rustls

server: remove take_chain() method from ClientCertDetails

rustls/src/server/tls12.rs
@@ -396,7 +396,7 @@ impl hs::State for ExpectCertificateVerify {
         }
 
         trace!("client CertificateVerify OK");
-        sess.client_cert_chain = Some(self.client_cert.take_chain());
+        sess.client_cert_chain = Some(self.client_cert.cert_chain);
 
         self.handshake
             .transcript

rustls/src/server/tls13.rs
@@ -825,7 +825,7 @@ impl hs::State for ExpectCertificateVerify {
         }
 
         trace!("client CertificateVerify OK");
-        sess.client_cert_chain = Some(self.client_cert.take_chain());
+        sess.client_cert_chain = Some(self.client_cert.cert_chain);
 
         self.handshake
             .transcript

lf-/clipper

rustls/src/server/tls12.rs
@@ -412,7 +412,7 @@ impl hs::State for ExpectCertificateVerify {
         }
 
         trace!("client CertificateVerify OK");
-        conn.client_cert_chain = Some(self.client_cert.take_chain());
+        conn.client_cert_chain = Some(self.client_cert.cert_chain);
 
         self.handshake
             .transcript

rustls/src/server/tls13.rs
@@ -845,7 +845,7 @@ impl hs::State for ExpectCertificateVerify {
         }
 
         trace!("client CertificateVerify OK");
-        conn.client_cert_chain = Some(self.client_cert.take_chain());
+        conn.client_cert_chain = Some(self.client_cert.cert_chain);
 
         self.handshake
             .transcript