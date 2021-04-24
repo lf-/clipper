rustls/rustls

quic: return error if QUIC params are missing
From draft-ietf-quic-tls-32#section-8.2:

   The quic_transport_parameters extension is carried in the ClientHello
   and the EncryptedExtensions messages during the handshake.  Endpoints
   MUST send the quic_transport_parameters extension; endpoints that
   receive ClientHello or EncryptedExtensions messages without the
   quic_transport_parameters extension MUST close the connection with an
   error of type 0x16d (equivalent to a fatal TLS missing_extension
   alert, see Section 4.8).

rustls/src/client/tls13.rs
@@ -444,8 +444,15 @@ impl hs::State for ExpectEncryptedExtensions {
         #[cfg(feature = "quic")]
         {
             // QUIC transport parameters
-            if let Some(params) = exts.get_quic_params_extension() {
-                conn.common.quic.params = Some(params);
+            if conn.common.is_quic() {
+                match exts.get_quic_params_extension() {
+                    Some(params) => conn.common.quic.params = Some(params),
+                    None => {
+                        return Err(conn
+                            .common
+                            .missing_extension("QUIC transport parameters not found"));
+                    }
+                }
             }
         }
 

rustls/src/server/hs.rs
@@ -137,9 +137,14 @@ impl ExtensionProcessing {
 
         #[cfg(feature = "quic")]
         {
-            if conn.common.protocol == Protocol::Quic {
-                if let Some(params) = hello.get_quic_params_extension() {
-                    conn.common.quic.params = Some(params);
+            if conn.common.is_quic() {
+                match hello.get_quic_params_extension() {
+                    Some(params) => conn.common.quic.params = Some(params),
+                    None => {
+                        return Err(conn
+                            .common
+                            .missing_extension("QUIC transport parameters not found"));
+                    }
                 }
 
                 if let Some(resume) = resumedata {

lf-/clipper

rustls/src/client/tls13.rs
@@ -434,8 +434,15 @@ impl hs::State for ExpectEncryptedExtensions {
         #[cfg(feature = "quic")]
         {
             // QUIC transport parameters
-            if let Some(params) = exts.get_quic_params_extension() {
-                cx.common.quic.params = Some(params);
+            if cx.common.is_quic() {
+                match exts.get_quic_params_extension() {
+                    Some(params) => cx.common.quic.params = Some(params),
+                    None => {
+                        return Err(cx
+                            .common
+                            .missing_extension("QUIC transport parameters not found"));
+                    }
+                }
             }
         }
 

rustls/src/server/hs.rs
@@ -162,9 +162,14 @@ impl ExtensionProcessing {
 
         #[cfg(feature = "quic")]
         {
-            if cx.common.protocol == Protocol::Quic {
-                if let Some(params) = hello.get_quic_params_extension() {
-                    cx.common.quic.params = Some(params);
+            if cx.common.is_quic() {
+                match hello.get_quic_params_extension() {
+                    Some(params) => cx.common.quic.params = Some(params),
+                    None => {
+                        return Err(cx
+                            .common
+                            .missing_extension("QUIC transport parameters not found"));
+                    }
                 }
 
                 if let Some(resume) = resumedata {