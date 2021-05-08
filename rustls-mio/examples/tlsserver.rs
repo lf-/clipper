sj6219/rustls

Improved builder-type API for making ServerConfigs

rustls-mio/examples/tlsserver.rs
@@ -572,8 +572,11 @@ fn make_config(args: &Args) -> Arc<rustls::ServerConfig> {
         NoClientAuth::new()
     };
 
-    let mut config = rustls::ServerConfig::new(client_auth);
-    config.key_log = Arc::new(rustls::KeyLogFile::new());
+    let suites = if !args.flag_suite.is_empty() {
+        lookup_suites(&args.flag_suite)
+    } else {
+        rustls::ALL_CIPHERSUITES.to_vec()
+    };
 
     let certs = load_certs(
         args.flag_certs
@@ -586,27 +589,28 @@ fn make_config(args: &Args) -> Arc<rustls::ServerConfig> {
             .expect("--key option missing"),
     );
     let ocsp = load_ocsp(&args.flag_ocsp);
-    config
-        .set_single_cert_with_ocsp_and_sct(certs, privkey, ocsp, vec![])
+
+    let mut config_builder = rustls::ServerConfigBuilder::new()
+        .with_ciphersuites(&suites)
+        .with_client_cert_verifier(client_auth)
+        .with_single_cert_with_ocsp_and_sct(certs, privkey, ocsp, vec![])
         .expect("bad certificates/private key");
 
-    if !args.flag_suite.is_empty() {
-        config.ciphersuites = lookup_suites(&args.flag_suite);
-    }
+    config_builder.set_key_log(Arc::new(rustls::KeyLogFile::new()));
 
     if !args.flag_protover.is_empty() {
-        config.versions = lookup_versions(&args.flag_protover);
+        config_builder.set_versions(&lookup_versions(&args.flag_protover));
     }
 
     if args.flag_resumption {
-        config.set_persistence(rustls::ServerSessionMemoryCache::new(256));
+        config_builder.set_persistence(rustls::ServerSessionMemoryCache::new(256));
     }
 
     if args.flag_tickets {
-        config.ticketer = rustls::Ticketer::new();
+        config_builder.set_ticketer(rustls::Ticketer::new());
     }
 
-    config.set_protocols(
+    config_builder.set_protocols(
         &args
             .flag_proto
             .iter()
@@ -614,7 +618,7 @@ fn make_config(args: &Args) -> Arc<rustls::ServerConfig> {
             .collect::<Vec<_>>()[..],
     );
 
-    Arc::new(config)
+    Arc::new(config_builder.build())
 }
 
 fn main() {

rustls/tests/common/mod.rs
@@ -8,10 +8,10 @@ use rustls::internal::pemfile;
 use rustls::ProtocolVersion;
 use rustls::Session;
 use rustls::TLSError;
-use rustls::{AllowAnyAuthenticatedClient, NoClientAuth, RootCertStore};
+use rustls::{AllowAnyAuthenticatedClient, RootCertStore};
 use rustls::{Certificate, PrivateKey};
 use rustls::{ClientConfig, ClientSession};
-use rustls::{ServerConfig, ServerSession};
+use rustls::{ServerConfig, ServerConfigBuilder, ServerSession};
 
 #[cfg(feature = "dangerous_configuration")]
 use rustls::{
@@ -199,11 +199,12 @@ impl KeyType {
 }
 
 pub fn make_server_config(kt: KeyType) -> ServerConfig {
-    let mut cfg = ServerConfig::new(NoClientAuth::new());
-    cfg.set_single_cert(kt.get_chain(), kt.get_key())
-        .unwrap();
-
-    cfg
+    ServerConfigBuilder::new()
+        .with_default_ciphersuites()
+        .without_client_auth()
+        .with_single_cert(kt.get_chain(), kt.get_key())
+        .unwrap()
+        .build()
 }
 
 pub fn get_client_root_store(kt: KeyType) -> RootCertStore {
@@ -219,12 +220,13 @@ pub fn make_server_config_with_mandatory_client_auth(kt: KeyType) -> ServerConfi
     let client_auth_roots = get_client_root_store(kt);
 
     let client_auth = AllowAnyAuthenticatedClient::new(client_auth_roots);
-    let mut cfg = ServerConfig::new(NoClientAuth::new());
-    cfg.set_client_certificate_verifier(client_auth);
-    cfg.set_single_cert(kt.get_chain(), kt.get_key())
-        .unwrap();
 
-    cfg
+    ServerConfigBuilder::new()
+        .with_default_ciphersuites()
+        .with_client_cert_verifier(client_auth)
+        .with_single_cert(kt.get_chain(), kt.get_key())
+        .unwrap()
+        .build()
 }
 
 pub fn make_client_config(kt: KeyType) -> ClientConfig {

lf-/clipper

rustls-mio/examples/tlsserver.rs
@@ -573,8 +573,11 @@ fn make_config(args: &Args) -> Arc<rustls::ServerConfig> {
         NoClientAuth::new()
     };
 
-    let mut config = rustls::ServerConfig::new(client_auth);
-    config.key_log = Arc::new(rustls::KeyLogFile::new());
+    let suites = if !args.flag_suite.is_empty() {
+        lookup_suites(&args.flag_suite)
+    } else {
+        rustls::ALL_CIPHERSUITES.to_vec()
+    };
 
     let certs = load_certs(
         args.flag_certs
@@ -587,33 +590,33 @@ fn make_config(args: &Args) -> Arc<rustls::ServerConfig> {
             .expect("--key option missing"),
     );
     let ocsp = load_ocsp(&args.flag_ocsp);
-    config
-        .set_single_cert_with_ocsp_and_sct(certs, privkey, ocsp, vec![])
+
+    let mut config = rustls::ServerConfigBuilder::new()
+        .with_cipher_suites(&suites)
+        .with_safe_default_kx_groups()
+        .with_client_cert_verifier(client_auth)
+        .with_single_cert_with_ocsp_and_sct(certs, privkey, ocsp, vec![])
         .expect("bad certificates/private key");
 
-    if !args.flag_suite.is_empty() {
-        config.cipher_suites = lookup_suites(&args.flag_suite);
-    }
+    config.key_log = Arc::new(rustls::KeyLogFile::new());
 
     if !args.flag_protover.is_empty() {
         config.versions = lookup_versions(&args.flag_protover);
     }
 
     if args.flag_resumption {
-        config.set_persistence(rustls::ServerSessionMemoryCache::new(256));
+        config.session_storage = rustls::ServerSessionMemoryCache::new(256);
     }
 
     if args.flag_tickets {
         config.ticketer = rustls::Ticketer::new().unwrap();
     }
 
-    config.set_protocols(
-        &args
-            .flag_proto
-            .iter()
-            .map(|proto| proto.as_bytes().to_vec())
-            .collect::<Vec<_>>()[..],
-    );
+    config.alpn_protocols = args
+        .flag_proto
+        .iter()
+        .map(|proto| proto.as_bytes().to_vec())
+        .collect::<Vec<_>>();
 
     Arc::new(config)
 }

rustls/tests/common/mod.rs
@@ -9,11 +9,11 @@ use rustls::internal::msgs::codec::Reader;
 use rustls::internal::msgs::message::{Message, OpaqueMessage};
 use rustls::Connection;
 use rustls::Error;
-use rustls::{AllowAnyAuthenticatedClient, NoClientAuth, RootCertStore};
+use rustls::{AllowAnyAuthenticatedClient, RootCertStore};
 use rustls::{Certificate, PrivateKey};
 use rustls::{ClientConfig, ClientConnection};
 use rustls::{ProtocolVersion, DEFAULT_CIPHERSUITES};
-use rustls::{ServerConfig, ServerConnection};
+use rustls::{ServerConfig, ServerConfigBuilder, ServerConnection};
 
 #[cfg(feature = "dangerous_configuration")]
 use rustls::{
@@ -222,11 +222,10 @@ impl KeyType {
 }
 
 pub fn make_server_config(kt: KeyType) -> ServerConfig {
-    let mut cfg = ServerConfig::new(NoClientAuth::new());
-    cfg.set_single_cert(kt.get_chain(), kt.get_key())
-        .unwrap();
-
-    cfg
+    ServerConfigBuilder::with_safe_default_crypto()
+        .with_no_client_auth()
+        .with_single_cert(kt.get_chain(), kt.get_key())
+        .unwrap()
 }
 
 pub fn get_client_root_store(kt: KeyType) -> RootCertStore {
@@ -242,12 +241,11 @@ pub fn make_server_config_with_mandatory_client_auth(kt: KeyType) -> ServerConfi
     let client_auth_roots = get_client_root_store(kt);
 
     let client_auth = AllowAnyAuthenticatedClient::new(client_auth_roots);
-    let mut cfg = ServerConfig::new(NoClientAuth::new());
-    cfg.set_client_certificate_verifier(client_auth);
-    cfg.set_single_cert(kt.get_chain(), kt.get_key())
-        .unwrap();
 
-    cfg
+    ServerConfigBuilder::with_safe_default_crypto()
+        .with_client_cert_verifier(client_auth)
+        .with_single_cert(kt.get_chain(), kt.get_key())
+        .unwrap()
 }
 
 pub fn make_client_config(kt: KeyType) -> ClientConfig {