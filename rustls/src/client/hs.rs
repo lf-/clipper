jbr/rustls

Add QUIC params to persist::Tls13ClientSessionValue
Prior to this, these parameters were being seperately appended
to the session value.  That meant Tls13ClientSessionValue was
misleading as a venue which owned the whole session value encoding.

rustls/src/client/hs.rs
@@ -9,8 +9,6 @@ use crate::kx;
 #[cfg(feature = "logging")]
 use crate::log::{debug, trace};
 use crate::msgs::base::Payload;
-#[cfg(feature = "quic")]
-use crate::msgs::base::PayloadU16;
 use crate::msgs::codec::{Codec, Reader};
 use crate::msgs::enums::{AlertDescription, Compression, ContentType};
 use crate::msgs::enums::{ECPointFormat, PSKKeyExchangeMode};
@@ -73,8 +71,9 @@ fn find_session(
         .and_then(|resuming| {
             #[cfg(feature = "quic")]
             if cx.common.is_quic() {
-                let params = PayloadU16::read(&mut reader)?;
-                cx.common.quic.params = Some(params.0);
+                cx.common.quic.params = resuming
+                    .tls13()
+                    .map(|v| v.get_quic_params());
             }
             Some(resuming)
         })

rustls/src/msgs/persist.rs
@@ -158,6 +158,8 @@ pub struct Tls13ClientSessionValue {
     age_add: u32,
     max_early_data_size: u32,
     pub common: ClientSessionCommon,
+    #[cfg(feature = "quic")]
+    quic_params: PayloadU16,
 }
 
 impl Tls13ClientSessionValue {
@@ -182,6 +184,8 @@ impl Tls13ClientSessionValue {
                 lifetime_secs,
                 server_cert_chain,
             ),
+            #[cfg(feature = "quic")]
+            quic_params: PayloadU16(Vec::new()),
         }
     }
 
@@ -195,6 +199,8 @@ impl Tls13ClientSessionValue {
             age_add: u32::read(r)?,
             max_early_data_size: u32::read(r)?,
             common: ClientSessionCommon::read(r)?,
+            #[cfg(feature = "quic")]
+            quic_params: PayloadU16::read(r)?,
         })
     }
 
@@ -211,6 +217,8 @@ impl Tls13ClientSessionValue {
         self.max_early_data_size
             .encode(&mut bytes);
         self.common.encode(&mut bytes);
+        #[cfg(feature = "quic")]
+        self.quic_params.encode(&mut bytes);
         bytes
     }
 
@@ -220,6 +228,16 @@ impl Tls13ClientSessionValue {
 
     pub fn suite(&self) -> &'static Tls13CipherSuite {
         self.suite
+    }
+
+    #[cfg(feature = "quic")]
+    pub fn set_quic_params(&mut self, quic_params: &[u8]) {
+        self.quic_params = PayloadU16(quic_params.to_vec());
+    }
+
+    #[cfg(feature = "quic")]
+    pub fn get_quic_params(&self) -> Vec<u8> {
+        self.quic_params.0.clone()
     }
 }
 

lf-/clipper

rustls/src/client/hs.rs
@@ -9,8 +9,6 @@ use crate::kx;
 #[cfg(feature = "logging")]
 use crate::log::{debug, trace};
 use crate::msgs::base::Payload;
-#[cfg(feature = "quic")]
-use crate::msgs::base::PayloadU16;
 use crate::msgs::codec::{Codec, Reader};
 use crate::msgs::enums::{AlertDescription, Compression, ContentType};
 use crate::msgs::enums::{ECPointFormat, PSKKeyExchangeMode};
@@ -73,8 +71,9 @@ fn find_session(
         .and_then(|resuming| {
             #[cfg(feature = "quic")]
             if cx.common.is_quic() {
-                let params = PayloadU16::read(&mut reader)?;
-                cx.common.quic.params = Some(params.0);
+                cx.common.quic.params = resuming
+                    .tls13()
+                    .map(|v| v.quic_params());
             }
             Some(resuming)
         })

rustls/src/msgs/persist.rs
@@ -161,6 +161,8 @@ pub struct Tls13ClientSessionValue {
     age_add: u32,
     max_early_data_size: u32,
     pub common: ClientSessionCommon,
+    #[cfg(feature = "quic")]
+    quic_params: PayloadU16,
 }
 
 impl Tls13ClientSessionValue {
@@ -185,6 +187,8 @@ impl Tls13ClientSessionValue {
                 lifetime_secs,
                 server_cert_chain,
             ),
+            #[cfg(feature = "quic")]
+            quic_params: PayloadU16(Vec::new()),
         }
     }
 
@@ -198,6 +202,8 @@ impl Tls13ClientSessionValue {
             age_add: u32::read(r)?,
             max_early_data_size: u32::read(r)?,
             common: ClientSessionCommon::read(r)?,
+            #[cfg(feature = "quic")]
+            quic_params: PayloadU16::read(r)?,
         })
     }
 
@@ -214,6 +220,8 @@ impl Tls13ClientSessionValue {
         self.max_early_data_size
             .encode(&mut bytes);
         self.common.encode(&mut bytes);
+        #[cfg(feature = "quic")]
+        self.quic_params.encode(&mut bytes);
         bytes
     }
 
@@ -223,6 +231,16 @@ impl Tls13ClientSessionValue {
 
     pub fn suite(&self) -> &'static Tls13CipherSuite {
         self.suite
+    }
+
+    #[cfg(feature = "quic")]
+    pub fn set_quic_params(&mut self, quic_params: &[u8]) {
+        self.quic_params = PayloadU16(quic_params.to_vec());
+    }
+
+    #[cfg(feature = "quic")]
+    pub fn quic_params(&self) -> Vec<u8> {
+        self.quic_params.0.clone()
     }
 }
 