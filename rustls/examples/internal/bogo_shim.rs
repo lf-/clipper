Herrgrim0/tcpls-basics

msgs: yield Result instead of Option from Reader/Codec

rustls/examples/internal/bogo_shim.rs
@@ -592,7 +592,7 @@ fn quit_err(why: &str) -> ! {
 }
 
 fn handle_err(err: rustls::Error) -> ! {
-    use rustls::{AlertDescription, ContentType};
+    use rustls::AlertDescription;
     use rustls::{Error, InvalidMessage, PeerMisbehaved};
     use std::{thread, time};
 
@@ -611,29 +611,30 @@ fn handle_err(err: rustls::Error) -> ! {
         Error::AlertReceived(AlertDescription::InternalError) => {
             quit(":PEER_ALERT_INTERNAL_ERROR:")
         }
-        Error::CorruptMessage(InvalidMessage::MissingPayload(ContentType::Alert)) => {
-            quit(":BAD_ALERT:")
-        }
-        Error::CorruptMessage(InvalidMessage::MissingPayload(ContentType::ChangeCipherSpec)) => {
-            quit(":BAD_CHANGE_CIPHER_SPEC:")
-        }
-        Error::CorruptMessage(InvalidMessage::MissingPayload(ContentType::Handshake)) => {
-            quit(":BAD_HANDSHAKE_MSG:")
-        }
-        Error::CorruptMessage(InvalidMessage::InvalidKeyUpdate(KeyUpdateRequest::Unknown(42))) => {
-            quit(":BAD_HANDSHAKE_MSG:")
-        }
+        Error::CorruptMessage(
+            InvalidMessage::MissingData("AlertDescription")
+            | InvalidMessage::TrailingData("AlertMessagePayload"),
+        ) => quit(":BAD_ALERT:"),
+        Error::CorruptMessage(
+            InvalidMessage::TrailingData("ChangeCipherSpecPayload")
+            | InvalidMessage::IncorrectFrame("ChangeCipherSpecPayload"),
+        ) => quit(":BAD_CHANGE_CIPHER_SPEC:"),
+        Error::CorruptMessage(
+            InvalidMessage::InvalidKeyUpdate(KeyUpdateRequest::Unknown(42))
+            | InvalidMessage::MissingData(_)
+            | InvalidMessage::TrailingData(_)
+            | InvalidMessage::IncorrectFrame("HelloRetryRequest")
+            | InvalidMessage::NoSignatureSchemes
+            | InvalidMessage::UnsupportedCompression,
+        ) => quit(":BAD_HANDSHAKE_MSG:"),
         Error::CorruptMessage(InvalidMessage::InvalidCertRequest)
         | Error::CorruptMessage(InvalidMessage::InvalidDhParams)
         | Error::CorruptMessage(InvalidMessage::MissingKeyExchange) => quit(":BAD_HANDSHAKE_MSG:"),
         Error::CorruptMessage(InvalidMessage::InvalidContentType)
         | Error::CorruptMessage(InvalidMessage::EmptyDataForDisallowedRecord)
         | Error::CorruptMessage(InvalidMessage::UnknownProtocolVersion)
-        | Error::CorruptMessage(InvalidMessage::MessageTooLarge)
-        | Error::CorruptMessage(InvalidMessage::MissingPayload(ContentType::Unknown(42))) => {
-            quit(":GARBAGE:")
-        }
-        Error::CorruptMessage(InvalidMessage::IncorrectFrame) => quit(":GARBAGE:"),
+        | Error::CorruptMessage(InvalidMessage::MessageTooLarge) => quit(":GARBAGE:"),
+        Error::CorruptMessage(InvalidMessage::IncorrectFrame(_)) => quit(":GARBAGE:"),
         Error::DecryptError => quit(":DECRYPTION_FAILED_OR_BAD_RECORD_MAC:"),
         Error::PeerIncompatible(_) => quit(":INCOMPATIBLE:"),
         Error::PeerMisbehaved(PeerMisbehaved::TooMuchEarlyDataReceived) => {

rustls/src/msgs/deframer.rs
@@ -2,6 +2,7 @@ use std::io;
 use std::ops::Range;
 
 use super::base::Payload;
+use super::codec::Codec;
 use super::enums::ContentType;
 use super::message::PlainMessage;
 use crate::error::{Error, PeerMisbehaved};
@@ -398,11 +399,11 @@ fn payload_size(buf: &[u8]) -> Result<Option<usize>, Error> {
     }
 
     let (header, _) = buf.split_at(HEADER_SIZE);
-    match codec::u24::decode(&header[1..]) {
-        Some(len) if len.0 > MAX_HANDSHAKE_SIZE => Err(Error::CorruptMessage(
+    match codec::u24::read_bytes(&header[1..]) {
+        Ok(len) if len.0 > MAX_HANDSHAKE_SIZE => Err(Error::CorruptMessage(
             InvalidMessage::HandshakePayloadTooLarge,
         )),
-        Some(len) => Ok(Some(HEADER_SIZE + usize::from(len))),
+        Ok(len) => Ok(Some(HEADER_SIZE + usize::from(len))),
         _ => Ok(None),
     }
 }

lf-/clipper

rustls/examples/internal/bogo_shim.rs
@@ -592,7 +592,7 @@ fn quit_err(why: &str) -> ! {
 }
 
 fn handle_err(err: rustls::Error) -> ! {
-    use rustls::{AlertDescription, ContentType};
+    use rustls::AlertDescription;
     use rustls::{Error, InvalidMessage, PeerMisbehaved};
     use std::{thread, time};
 
@@ -611,22 +611,29 @@ fn handle_err(err: rustls::Error) -> ! {
         Error::AlertReceived(AlertDescription::InternalError) => {
             quit(":PEER_ALERT_INTERNAL_ERROR:")
         }
-        Error::InvalidMessage(InvalidMessage::MissingPayload(ContentType::Alert)) => {
-            quit(":BAD_ALERT:")
-        }
-        Error::InvalidMessage(InvalidMessage::MissingPayload(ContentType::ChangeCipherSpec)) => {
-            quit(":BAD_CHANGE_CIPHER_SPEC:")
-        }
-        Error::InvalidMessage(InvalidMessage::MissingPayload(ContentType::Handshake)) => {
-            quit(":BAD_HANDSHAKE_MSG:")
-        }
-        Error::InvalidMessage(InvalidMessage::InvalidKeyUpdate(KeyUpdateRequest::Unknown(42))) => {
-            quit(":BAD_HANDSHAKE_MSG:")
-        }
+        Error::InvalidMessage(
+            InvalidMessage::MissingData("AlertDescription")
+            | InvalidMessage::TrailingData("AlertMessagePayload"),
+        ) => quit(":BAD_ALERT:"),
+        Error::InvalidMessage(
+            InvalidMessage::TrailingData("ChangeCipherSpecPayload") | InvalidMessage::InvalidCcs,
+        ) => quit(":BAD_CHANGE_CIPHER_SPEC:"),
+        Error::InvalidMessage(
+            InvalidMessage::InvalidKeyUpdate(KeyUpdateRequest::Unknown(42))
+            | InvalidMessage::MissingData(_)
+            | InvalidMessage::TrailingData(_)
+            | InvalidMessage::UnexpectedMessage("HelloRetryRequest")
+            | InvalidMessage::NoSignatureSchemes
+            | InvalidMessage::UnsupportedCompression,
+        ) => quit(":BAD_HANDSHAKE_MSG:"),
         Error::InvalidMessage(InvalidMessage::InvalidCertRequest)
         | Error::InvalidMessage(InvalidMessage::InvalidDhParams)
         | Error::InvalidMessage(InvalidMessage::MissingKeyExchange) => quit(":BAD_HANDSHAKE_MSG:"),
-        Error::InvalidMessage(_) => quit(":GARBAGE:"),
+        Error::InvalidMessage(InvalidMessage::InvalidContentType)
+        | Error::InvalidMessage(InvalidMessage::InvalidEmptyPayload)
+        | Error::InvalidMessage(InvalidMessage::UnknownProtocolVersion)
+        | Error::InvalidMessage(InvalidMessage::MessageTooLarge) => quit(":GARBAGE:"),
+        Error::InvalidMessage(InvalidMessage::UnexpectedMessage(_)) => quit(":GARBAGE:"),
         Error::DecryptError => quit(":DECRYPTION_FAILED_OR_BAD_RECORD_MAC:"),
         Error::PeerIncompatible(_) => quit(":INCOMPATIBLE:"),
         Error::PeerMisbehaved(PeerMisbehaved::TooMuchEarlyDataReceived) => {

rustls/src/msgs/deframer.rs
@@ -2,6 +2,7 @@ use std::io;
 use std::ops::Range;
 
 use super::base::Payload;
+use super::codec::Codec;
 use super::enums::ContentType;
 use super::message::PlainMessage;
 use crate::enums::ProtocolVersion;
@@ -396,11 +397,11 @@ fn payload_size(buf: &[u8]) -> Result<Option<usize>, Error> {
     }
 
     let (header, _) = buf.split_at(HEADER_SIZE);
-    match codec::u24::decode(&header[1..]) {
-        Some(len) if len.0 > MAX_HANDSHAKE_SIZE => {
-            Err(InvalidMessage::HandshakePayloadTooLarge.into())
-        }
-        Some(len) => Ok(Some(HEADER_SIZE + usize::from(len))),
+    match codec::u24::read_bytes(&header[1..]) {
+        Ok(len) if len.0 > MAX_HANDSHAKE_SIZE => Err(Error::InvalidMessage(
+            InvalidMessage::HandshakePayloadTooLarge,
+        )),
+        Ok(len) => Ok(Some(HEADER_SIZE + usize::from(len))),
         _ => Ok(None),
     }
 }